aa50cbc59a0b8465854be1b74a11cb922c8dfcc5
piguet-anthony <listeanthony@gmail.com>
2022-07-08 10:58:52 +0200

    api key

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
+
 const bodyParser = require('body-parser');
-
-
 const routes = require('./routes/routes');
-
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const HTTP_PORT = 8000;
+const API_KEY = "Bearer 8f94826adab8ffebbeadb4f9e161b2dc";
 
 app.listen(HTTP_PORT, () => {
     console.log(`Server running on port ${HTTP_PORT}`);
@@ -19,5 +19,15 @@ app.get('/',(req, res) => {
     res.json({ message: 'Hello World' });
 });
 
-// Routes "genre"
+// PROTECT ALL ROUTES THAT FOLLOW
+app.use((req, res, next) => {
+    const apiKey = req.get('Authorization')
+    if (!apiKey || apiKey !== API_KEY) {
+      res.status(401).json({error: 'Votre API KEY n\'est pas valide ou inexistante'})
+    } else {
+      next()
+    }
+  })
+
+// Routes 
 app.use('/api', routes);
